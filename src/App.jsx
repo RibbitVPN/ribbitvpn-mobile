ecc210cc702a65f7ffa883353793f988039baa8b
Anjo2807 <anjodima54@gmail.com>
2025-04-16 18:22:41 +0200

    vpn buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import './App.css'
 import ribbitlogo from './assets/ribbitlogo.png'
 
 export function App() {
-
   return (
     <view>
         <text className='Title'>RibbitVPN</text>
+        <text className='vpnbutton'>On</text>
     </view>
   )
 }
