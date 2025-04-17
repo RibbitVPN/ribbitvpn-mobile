7f0fe8966074efb4684c6f8b96bd84f9566b4cce
Anjo2807 <anjodima54@gmail.com>
2025-04-17 10:14:37 +0200

    anyways remove useless stuff and make button work (changing colors coming soon)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { useCallback, useEffect, useState } from '@lynx-js/react'
+import { useState } from '@lynx-js/react'
 
 import './App.css'
 import bg from './assets/bg.jpg'
 
 export function App() {
+  let [vpnstatus, setVpnStatus] = useState(false)
+  let [vpntext, setVpnText] = useState('Off')
+
+  const touch = () => { 
+    if (vpnstatus) {
+      setVpnStatus(false);
+      setVpnText('Off');
+    } else {
+      setVpnStatus(true);
+      setVpnText('On');
+    }
+  }
   return (
     <view>
         <text className='Title'>RibbitVPN</text>
-        <view className='vpnbutton'></view>
+        <view id="vpnbutton" className='vpnbutton' bindtap={touch}><text className="vpnstatustext">{vpntext}</text></view>
     </view>
   )
 }
