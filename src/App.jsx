211eb456b44bb9cb805f8228c21571f834d1c53b
Anjo2807 <anjodima54@gmail.com>
2025-04-20 12:54:42 +0200

    buttons change color :D

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,23 @@ import bg from './assets/bg.jpg'
 export function App() {
   let [vpnstatus, setVpnStatus] = useState(false)
   let [vpntext, setVpnText] = useState('Off')
+  let [vpnbuttonstatus, setVpnButtonStatus] = useState('vpnbutton-off')
 
   const touch = () => { 
     if (vpnstatus) {
       setVpnStatus(false);
       setVpnText('Off');
+      setVpnButtonStatus('vpnbutton-off');
     } else {
       setVpnStatus(true);
       setVpnText('On');
+      setVpnButtonStatus('vpnbutton-on');
     }
   }
   return (
     <view>
         <text className='Title'>RibbitVPN</text>
-        <view id="vpnbutton" className='vpnbutton' bindtap={touch}><text className="vpnstatustext">{vpntext}</text></view>
+        <view id="vpnbutton" className={vpnbuttonstatus} bindtap={touch}><text className="vpnstatustext">{vpntext}</text></view>
     </view>
   )
 }
